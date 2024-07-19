90d7e2c4e62aeecf98ea47e59fec853ccf0b6cfe
kacper-holowaty <kacper.holowaty@gmail.com>
2024-07-19 14:38:05 +0200

    nadrabianie zaległości - lab06

--- lab06/zad_06.py
+++ lab06/zad_06.py
@@ -6,4 +6,22 @@
 # W przeciwieństwie do innych metod, YOLO dokonuje detekcji obiektów w jednym przebiegu sieci neuronowej, 
 # co prowadzi do znacznie szybszej detekcji.
 
-# TO DO
+from imageai.Detection import ObjectDetection  
+
+recognizer = ObjectDetection()  
+path_model = "./models/tiny-yolov3.pt"  
+path_input = "./input/image.jpg"  
+path_output = "./output/detected_objects.jpg"  
+
+recognizer.setModelTypeAsTinyYOLOv3()  
+recognizer.setModelPath(path_model)  
+recognizer.loadModel()  
+
+recognition = recognizer.detectObjectsFromImage(  
+    input_image = path_input,  
+    output_image_path = path_output  
+    )  
+
+for item in recognition:  
+    print(item["name"] , " : ", item["percentage_probability"])  
+
